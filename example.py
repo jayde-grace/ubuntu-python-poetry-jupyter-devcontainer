c214ac0011cc07e96fedd658c7098c18a162c6b4
Jayde Grace <jayde.grace@icloud.com>
2024-11-10 05:18:39 +0000

    first commit

---
+++ example.py
@@ -0,0 +1,18 @@
+# Create python executable file
+
+# Import libraries
+import pandas as pd
+import numpy as np
+
+# Define functions
+
+# Define main function
+def main():
+    array = np.array([1, 2, 3, 4, 5])
+    df = pd.DataFrame(array)
+    print("Pandas and Numpy imported successfully!")
+
+# Run main function
+if __name__ == "__main__":
+    main()
+    
